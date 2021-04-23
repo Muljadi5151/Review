9eafa30d9eb1d8ef9d7d42177291f8e25dc1511f
Muljadi5151 <muljadiyadi29@gmail.com>
2021-04-23 18:53:49 +0700

    project demo abstrak

---
+++ Review/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pertambahan a1 = new Pertambahan(1, 2);
+            Console.WriteLine(a1.Proccess());
+            a1.Display();
+            Pengurangan a2 = new Pengurangan(10, 5);
+            Console.WriteLine(a2.Proccess());
+            a2.Display();
+            Perkalian a3 = new Perkalian(2, 5);
+            Console.WriteLine(a3.Proccess());
+            a3.Display();
+            Pembagian a4 = new Pembagian(10, 5);
+            Console.WriteLine(a4.Proccess());
+            a4.Display();
+            Console.Read();
+        }
+        class Pertambahan : DemoAbstract
+        {
+            public Pertambahan(int a, int b)
+            {
+                A = a;
+                B = b;
+            }
+            public int A { get; private set; }
+            public int B { get; private set; }
+
+            public override int Proccess()
+            {
+                return A + B;
+            }
+        }
+        class Pengurangan : DemoAbstract
+        {
+            public Pengurangan(int c, int d)
+            {
+                C = c;
+                D = d;
+            }
+            public int C { get; private set; }
+            public int D { get; private set; }
+            public override int Proccess()
+            {
+                return C - D;
+            }
+        }
+        class Perkalian : DemoAbstract
+        {
+            public Perkalian(int c, int d)
+            {
+                C = c;
+                D = d;
+            }
+            public int C { get; private set; }
+            public int D { get; private set; }
+            public override int Proccess()
+            {
+                return C * D;
+            }
+        }
+        class Pembagian : DemoAbstract
+        {
+            public Pembagian(int c, int d)
+            {
+                C = c;
+                D = d;
+            }
+            public int C { get; private set; }
+            public int D { get; private set; }
+            public override int Proccess()
+            {
+                return C / D;
+            }
+        }
+    }
+}
---
+++ Review/hitungPenjumlahan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review
+{
+    class hitungPenjumlahan : Hitung
+    {
+        private int a, b, c;
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                b = value;
+            }
+        }
+        public int C
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                c = value;
+            }
+        }
+        public void Penjumlahan()
+        {
+            C = A + B;
+            Console.WriteLine("Penumlahan dari " + A + " dan " + B + " adalah " + C);
+        }
+    }
+}
